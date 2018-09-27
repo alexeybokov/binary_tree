a072a5918fa4a1303248c04629a4332c33b00e70
alexeybokov <lexabokov18@gmail.com>
2018-09-27 10:39:15 +0300

    add menu

--- index.rb
+++ index.rb
@@ -2,19 +2,45 @@ require_relative 'navigation'
 require_relative 'node'
 require_relative 'binary_tree'
 
-tree = BinaryTree.new(50)
-tree.add(100)
-tree.add(30)
-tree.add(40)
-tree.add(60)
-tree.add(88)
-tree.add(10)
-tree.add(55)
-tree.add(5)
-puts "Tree nodes: #{tree.to_s}"
-tree.navigation
-tree.remove_node_with_children(60)
-puts "Tree nodes: #{tree.to_s}"
-tree.remove_node_with_children(30)
-puts "Tree nodes: #{tree.to_s}"
+@current_tree = BinaryTree.new(0)
 
+loop do
+  puts '___________________________________________________'
+  puts 'Allowed commands:'
+  puts '    1 - Create binary-tree'
+  puts '    2 - Add new item'
+  puts '    3 - Remove item ( its delete all child of item )'
+  puts '    4 - Start navigation for binary-tree'
+  puts '    5 - Exit'
+
+  command = gets.chomp
+  unless (1..5).to_a.include? command.to_i
+    puts "Wrong command - #{command}"
+    next
+  end
+
+  case command.to_i
+  when 1
+    puts 'For create binary-tree puts first item(root):'
+    root = gets.chomp
+    binary_tree = BinaryTree.new(root)
+    @current_tree = binary_tree
+    puts "Now your binary-tree: #{binary_tree.to_s}"
+  when 2
+    puts 'Enter item for add to binary-tree:'
+    item = gets.chomp
+    @current_tree.add(item)
+    puts "Inserting #{item}......"
+    puts "Now your binary-tree: #{@current_tree.to_s}"
+  when 3
+    puts 'Enter item for delete (also delete items children if they are):'
+    r_item = gets.chomp
+    @current_tree.remove_node_with_children(r_item)
+    puts "Now your binary-tree: #{@current_tree.to_s}"
+  when 4
+    @current_tree.navigation
+  when 5
+    puts ' Good Bye!'
+    return
+  end
+end
